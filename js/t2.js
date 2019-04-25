d54439fdc563f9167d20cd8e45683db151b94e91
hastigh <hasti6553@yahoo.com>
2019-04-25 19:20:32 +0430

    Initialize project

---
+++ js/t2.js
@@ -0,0 +1,16 @@
+document.getElementById("one").addEventListener("click", function() {
+let ONE1=document.getElementById("one").innerHTML;
+document.getElementById("ans").textContent = ONE1;
+});
+document.getElementById("2").addEventListener("click", function() {
+    let two=document.getElementById("2").innerHTML;
+    document.getElementById("ans").textContent = two;
+    });
+    document.getElementById("3").addEventListener("click", function() {
+        let th=document.getElementById("3").innerHTML;
+        document.getElementById("ans").textContent = th;
+        });
+        document.getElementById("4").addEventListener("click", function() {
+            let f=document.getElementById("4").innerHTML;
+            document.getElementById("ans").textContent = f;
+            });
